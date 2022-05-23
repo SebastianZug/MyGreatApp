fbbe3634f84d6e65838ac95c1fd62169f85b3fd4
Sebastian Zug <sebastian.zug@informatik.tu-freiberg.de>
2022-05-23 11:48:47 +0200

    Add new outputline from branch

--- MyFancyApp/Program.cs
+++ MyFancyApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, this was changed on remote server!");
+Console.WriteLine("Hello, this was changed in separate branch!");
